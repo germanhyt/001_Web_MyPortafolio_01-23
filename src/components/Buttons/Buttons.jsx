3dc494650c9d0d5ef2b87f089d97b5a8fbf55187
germanhyt <85075441+germanhyt@users.noreply.github.com>
2023-01-09 14:52:03 -0500

    Add

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -21,4 +21,6 @@ export default Buttons;
 // //Nuetros doc en drive:
 // https://drive.google.com/file/d/1P9dMn0s5SnD69oMBqYQSS3g6c1wEz0T2/view?usp=sharing
 // //Ejemplo compartido:
-// https://drive.google.com/uc?export=download&id=1P9dMn0s5SnD69oMBqYQSS3g6c1wEz0T2
+// https://drive.google.com/uc?export=download&id=14dEih51j5w4u187redk4OklUWRUITrP-
+
+
